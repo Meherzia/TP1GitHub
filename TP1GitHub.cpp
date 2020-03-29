8e456a946f4571dc737d4081d809cc8cc023c134
Jimmy <jngu93@gmail.com>
2020-03-28 20:07:24 -0400

    jimmy merge

--- TP1GitHub.cpp
+++ TP1GitHub.cpp
@@ -1,20 +1,12 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int Div, min, PGCD=1,num,denom,Snum,Sdenom;
-//Diviseur,minimum,PGCD initialisé a 1,numerateur,denominateur,numerateur simplifié,denominateuyr simplifié
-int calculPGCD(int num, int denom);// fonction qui calcule le PGCD et retourne un entier:le PGCD
-void Simplification(int num, int denom);//fonction qui fait la simplification du numerateur et du denominateur
-int main()
-{
-	
-	Simplification(num, denom);//appel de la foction simplification
-	
-	
-}
+
 int calculPGCD(int num, int denom) {
 	//Trouver le minimum de num et denom
+	int min, Div, PGCD;
 	if (num < denom) {
-		min = nom;
+		min = num;
 	}
 	else {
 		min = denom;
@@ -29,17 +21,68 @@ int calculPGCD(int num, int denom) {
 	}
 	cout << "Le plus grand diviseur en commun entre A et B est :" << PGCD << endl;
 	return PGCD;
-	
-}	
-void Simplification(int num, int denum) {
-		cout << "saisir le numerateur:" << endl;//demander a l'utilisateur de saisir le numerateur
-		cin >> num;//lire le numerateur 
-		cout << "saisir le denominateur:" << endl;//demander a l'utilisateur de saisir le denominateur
-		cin >> denom;//lire le denominbateur
-		PGCD = calculPGCD(num, denom);//apple de la fonction qui calcule le PGCD
-		Snum = num / PGCD;//	diviser le nummerateur par le PGCD pour le simplifier
-		Sdenum = denom / PGCD;// diviser le denominateur par le PGCD pour le simplifier
-		cout << "La simplification de la fraction est :" << Snum << "/" << Sdenum << endl;
-		//afficher la fraction simplifiée
 }
 
+void Simplification(int num, int denum) {
+	cout << "saisir le numerateur:" << endl;//demander a l'utilisateur de saisir le numerateur
+	cin >> num;//lire le numerateur 
+	cout << "saisir le denominateur:" << endl;//demander a l'utilisateur de saisir le denominateur
+	cin >> denom;//lire le denominbateur
+	PGCD = calculPGCD(num, denom);//apple de la fonction qui calcule le PGCD
+	Snum = num / PGCD;//	diviser le nummerateur par le PGCD pour le simplifier
+	Sdenum = denom / PGCD;// diviser le denominateur par le PGCD pour le simplifier
+	cout << "La simplification de la fraction est :" << Snum << "/" << Sdenum << endl;
+	//afficher la fraction simplifiée
+}
+
+void print(int f, vector<int> numerateur, vector<int> denominateur) {
+	int i, PGCD;
+	for (i = 0; i <= f - 1; i++) {
+
+		PGCD = calculPGCD(numerateur[i], denominateur[i]);
+
+		cout << numerateur[i] << '/' << denominateur[i] << " ---> " << numerateur[i] / PGCD << '/' << denominateur[i] / PGCD << endl;
+	}
+}
+
+void fctQte()
+{
+	cout << "Saisir la qte de fractions a analyser: ";
+	int i, f, num = 0, denom = 0;
+	vector<int> numerateur, denominateur;
+
+	cin >> f;
+	for (i = 1; i <= f; i++) {
+		cout << "Entrer le numerateur " << i << "\n";
+		cin >> num;
+		if (num > 0) {
+			numerateur.push_back(num);
+		}
+		else {
+			cout << "Le numerateur doit etre plus grand que zero! \n";
+			cout << "Entrer un autre numerateur: " << "\n";
+			cin >> num;
+			numerateur.push_back(num);
+		}
+
+		cout << "Entrer le denominateur " << i << "\n";
+		cin >> denom;
+		if (denom > 0) {
+			denominateur.push_back(denom);
+		}
+		else {
+			cout << "Le denominateur doit etre plus grand que zero! \n";
+			cout << "Entrer un autre denominateur: " << "\n";
+			cin >> denom;
+			denominateur.push_back(denom);
+		}
+	}
+
+	print(f, numerateur, denominateur);
+}
+
+int main() {
+
+	fctQte();
+	//Simplification(num, denom);//appel de la fonction simplification
+}
